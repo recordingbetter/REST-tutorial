f888ae314cb4cc6c1db53296eb0d56708b402c4b
Joe Donghee Cho <recordingbetter@gmail.com>
2017-06-28 20:10:24 +0900

    add snippets.serializers

---
+++ django-app/snippets/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+
+from snippets.models import LANGUAGE_CHOICES, STYLE_CHOICES, Snippet
+
+
+class SnippetSerializer(serializers.Serializer):
+    pk = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
+    code = serializers.CharField(style={'base_template': 'textarea.html'})
+    linenos = serializers.BooleanField(required=False)
+    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
+    style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+
+    def create(self, validated_data):
+        """
+        검증한 데이터로 새 `Snippet` 인스턴스를 생성하여 리턴합니다.
+        :param validated_data: 검증된 데이터
+        :return: Snippet object
+        """
+        return Snippet.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        검증한 데이터로 `Snippet` 인스턴스를 업데이트한 후 리턴합니다.
+        :param instance: Snippet instance
+        :param validated_data: 검증된 데이터
+        :return: Snippet object
+        """
+        instance.title = validated_data.get('title', instance.title)
+        instance.code = validated_data.get('code', instance.code)
+        instance.linenos = validated_data.get('linenos', instance.linenos)
+        instance.language = validated_data.get('language', instance.language)
+        instance.style = validated_data.get('style', instance.style)
+        instance.save()
+        return instance
